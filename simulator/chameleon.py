11f7534313e87a32cf0844620cdf0d5a1e438857
davenpi <iandavenport@g.harvard.edu>
2021-12-31 08:37:20 -0500

    go back to constant grid as that is more principled. Also edit the structure of rewards in step method to set up RL pipeline

--- simulator/chameleon.py
+++ simulator/chameleon.py
@@ -12,7 +12,9 @@ import forward_helpers as fh
 class Chameleon(gym.Env):
     def __init__(
         self,
-        init_pos: np.ndarray = np.linspace(0, 1, 50),  # hardcoding for now
+        init_pos: np.ndarray = np.linspace(
+            0, 1, 50
+        ),  # hardcoding for now. also in reset method
         current_disp: np.ndarray = np.zeros(50),
         target_pos: float = 2,
         E: float = 1,
@@ -27,7 +29,7 @@ class Chameleon(gym.Env):
         self.alpha = alpha
         self.pos_0 = init_pos
         self.pos_f = self.pos_0
-        self.disp_current = current_disp  # can be different than pos_f - pos_0
+        self.disp_current = current_disp  # can be different than pos_f - pos_0 b/c i could be recreating env from a previous end point
         self.dt = dt
         self.with_grad = with_grad
         self.n_steps = 1000
@@ -41,9 +43,12 @@ class Chameleon(gym.Env):
         self.action_space = gym.spaces.Box(low=0, high=1, shape=(3,))
         # observation space is just going to consist of tip of tongue
         self.observation_space = gym.spaces.Box(low=0, high=10, shape=(1,))
+        self.step_counter = 0
+        self.ep_length = 10
 
     def step(
-        self, action: np.ndarray, **sim_steps
+        self,
+        action: np.ndarray,
     ) -> Tuple[np.ndarray, float, bool, dict]:
         """
         Move the simulation forward one step in time by upddating the position
@@ -52,19 +57,14 @@ class Chameleon(gym.Env):
         Parameters
         ----------
         action : np.ndarray
-            action used to specify active torque. Right now they are the
-            coefficients of the constant, linear, and quadratic terms in
-            the stress.
-
-        kwargs
-        ------
-        sim_steps : int
-            Optional keyword argument to specify how many steps to simulate
-            for.
+            Action used to specify active torque. Right now this array should
+            be length three and contain the coefficients of the constant,
+            linear, and quadratic terms in the stress. Each coefficient must
+            lie in the range (0, 1).
 
         Returns
         -------
-        state : float
+        state : np.ndarray
             State of system after step. Just the tip position.
         reward : float
             Reward computed after the step is over.
@@ -74,38 +74,52 @@ class Chameleon(gym.Env):
         info : dict
             Logging info.
         """
-        steps = sim_steps.get("sim_steps")
         constant = action[0] * np.ones(self.n_elems)
         linear = action[1] * self.pos_f
         quadratic = action[2] * self.pos_f ** 2
-        active_stress = constant + linear + quadratic
-        print(action)
-        print(active_stress)
+        active_stress = 2 * (constant + linear + quadratic)
         done = False
-        # try:
-        fh.forward_simulate(self, active_stress, sim_steps=steps)
-        state = np.array([self.pos_f[-1]], dtype=np.float32)
-        # get negative reward for distance and one negative reward for time
-        reward = -np.abs(state - self.target_pos).item() - 1
-        if state.item() == self.target_pos:
+        try:
+            fh.forward_simulate(self, active_stress, sim_steps=1_000)
+            state = np.array([self.pos_f[-1]], dtype=np.float32)
+            close = np.isclose(state.item(), self.target_pos)
+            self.step_counter += 1
+        except:
+            print(
+                "elements likely came out of order"
+            )  # terrible feedback. be more specific and differentiate
+            done = True
+            reward = -1000
+            self.reset()
+
+        if close:
             done = True
             # negative reward for large velocity at the end. want to reach
             # target and be slowing down or nearly stopped
-            reward -= (
-                self.position_history[-1][-1] - self.position_history[-2][-1]
-            ) / self.dt
+            print(f"Reached in {self.step_counter} steps! =)")
+            reward -= (self.pos_f[-1] - self.position_history[-2][-1]) / self.dt
             self.reset()
-        # except:
-        #     print("Simulation messed up somewhere!")
-        #     reward = -1000.0
-        #     done = True
-        #     state = np.array([self.pos_f[-1]])
-        #     self.reset()
+        elif (state.item() > self.target_pos) or (
+            self.step_counter > self.ep_length
+        ):  # fail for overshooting or taking too long
+            print("overshot target or took too long =(")
+            done = True
+            reward = -1000
+            self.reset()
+        else:  # get negative reward for distance and one negative reward for time
+            print("Trying to reach! =|")
+            reward = -np.abs(state - self.target_pos).item() - 1
         return state, reward, done, {}
 
     def reset(self):
+        self.step_counter = 0
         self.position_history.clear()
+        self.pos_0 = np.ndarray = np.linspace(0, 1, 50)
         self.pos_f = self.pos_0
+        self.position_history.append(self.pos_0)
+        self.displacement_history.clear()
+        self.disp_current = self.pos_f - self.pos_0
+        self.displacement_history.append(self.disp_current)
         state = np.array([self.pos_f[-1]], dtype=np.float32)
         return state
 
