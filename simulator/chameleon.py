fad2de8375f84909d16b0404d6d4b67095f6bf49
davenpi <iandavenport@g.harvard.edu>
2022-01-18 10:37:17 -0500

    add reward structure to do reaching out and returning. PPO agent can solve the task but the reward is not clear enough to solve it smoothly. more reward engineering needed.

--- simulator/chameleon.py
+++ simulator/chameleon.py
@@ -21,17 +21,21 @@ class Chameleon(gym.Env):
         self.alpha = alpha
         self.n_elems = n_elems
         self.target_pos = target_pos
+        self.original_target_pos = target_pos
         self.dt = dt
         self.pos_init = np.linspace(0, init_length, self.n_elems)
         self.dx = self.pos_init[1] - self.pos_init[0]
         self.pos = copy.deepcopy(self.pos_init)
         self.u_current = self.pos - self.pos_init
-        self.active_stress_hist = deque([], maxlen=10)
-        self.active_stress_hist.append(np.zeros(self.n_elems))
+
         self.observation_space = gym.spaces.Box(low=0, high=1, shape=(1,))
         self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,))
         self.learning_counter = 0
-        self.episode_length = 25
+        self.episode_length = 50
+        self.active_stress_hist = deque([], maxlen=self.episode_length + 1)
+        self.active_stress_hist.append(np.zeros(self.n_elems))
+        self.winning_stress_hist = deque([], maxlen=self.episode_length + 1)
+        ### ADD IN METHOD WHICH ENSURES TARGET POS IS WITHIN OBSERVATION SPACE
 
     def one_step(
         self, active_stress_curr: np.ndarray, active_stress_prev: np.ndarray
@@ -55,8 +59,8 @@ class Chameleon(gym.Env):
 
     def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, dict]:
         const = action[0] * np.ones(self.n_elems)
-        linear = action[1] * self.pos
-        # quad = action[2] * self.pos ** 2
+        linear = action[1] * self.pos_init
+        # quad = action[2] * self.pos_init ** 2
         active_stress = const + linear  # + quad
 
         for i in range(1):  # take this many steps per learning step
@@ -66,25 +70,43 @@ class Chameleon(gym.Env):
 
         self.learning_counter += 1
         diffs = np.diff(self.pos)
-        if any(diffs < 0):  # end sim if elements out of order
+        if any(diffs < 0):
             state = self.reset()
             reward = -1000
             done = True
             print("OOO")
         else:
             state = np.array([self.pos[-1]], dtype=np.float32)
+            out_of_bounds = state.item() > self.observation_space.high.item()
             overtime = self.learning_counter > self.episode_length
-            close = np.isclose(state.item(), self.target_pos, rtol=0.05)
+            close = np.isclose(state.item(), self.target_pos, rtol=0.015)
             if overtime:
+                print("OOT")
                 state = self.reset()
+                reward = -1000
                 done = True
-                reward = -1
+            elif out_of_bounds:
+                print("OOB")
+                state = self.reset()
+                reward = -1000
+                done = True
             elif close:
-                print(f"Solved! in {self.learning_counter} steps")
-                self.winning_pos = self.pos  # kludge to get last position
-                state = self.reset()
-                done = True
-                reward = 0
+                if self.target_pos == self.pos_init[-1]:
+                    print(
+                        f"Returned! in {self.learning_counter} steps to {state.item()}"
+                    )
+                    self.winning_pos = self.pos  # kludge to get last position
+                    self.winning_stress_hist = copy.copy(self.active_stress_hist)
+                    state = self.reset()
+                    reward = 1000
+                    done = True
+                else:  # reward for reaching first target and update target position
+                    print(
+                        f"Reahced! In {self.learning_counter} steps to {state.item()}"
+                    )
+                    self.target_pos = self.pos_init[-1]
+                    reward = -1
+                    done = False
             else:
                 reward = -1
                 reward -= np.abs(
@@ -102,6 +124,7 @@ class Chameleon(gym.Env):
         self.active_stress_hist.append(np.zeros(self.n_elems))
         state = np.array([self.pos[-1]], dtype=np.float32)
         self.learning_counter = 0
+        self.target_pos = self.original_target_pos
         return state
 
     def render():
--- visualize.py
+++ visualize.py
@@ -10,7 +10,7 @@ with open("simulator/winning_history.txt", "rb") as fp:
 h = np.array(history)
 fig = plt.figure(figsize=(7, 7))
 ax = plt.axes(xlim=(0, h.max() + 0.1), ylim=(-0.2, 0.2))
-ax.scatter(0.2, 0)  # this is really dumb. need to automate this
+ax.scatter(0.18, 0)  # this is really dumb. need to automate this
 scatter = ax.scatter(history[0][:, 0], history[0][:, 1])
 
 
@@ -21,5 +21,5 @@ def update(frame_number):
 
 anim = FuncAnimation(fig, update, interval=10, frames=len(history))
 writervideo = animation.FFMpegWriter(fps=3)
-anim.save("updated_viz.mp4", writer=writervideo)
+anim.save("reach_return_viz.mp4", writer=writervideo)
 plt.close()
